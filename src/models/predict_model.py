8715b64979be0e3ecc393fd64d12f06d8d50a6e4
Brooke Kennedy <bck.007@gmail.com>
2021-02-26 05:13:17 -0600

    Finish modeling

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,10 +3,51 @@ import logging
 import numpy as np
 import os
 import pandas as pd
+import pickle
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from train_model import prepare_data
+
+
+def load_pickle(filename, path='../../models'):
+    """Returns pickle file
+
+    Args:
+        filename (str): filename of pickle to load
+        path (str): path to pickle file
+    Returns:
+        loaded pickle object
+
+    """
+    with open(os.path.join(pickle_path, filename), 'rb') as file:  
+        my_pickle = pickle.load(file)
+    file.close()
+
+    return my_pickle
+
+
+def get_missing_cols(train_columns, X):
+    """Make sure test set has same columns as training set
+
+    Args:
+        train_columns (list): list of column names used in training
+        X (dataframe): test dataframe
+    Returns:
+        Aligned test dataframe
+
+    """
+    # get missing columns in the training set
+    missing_cols = set(train_columns) - set(X.columns)
+    # add a missing column in test set as missing
+    for c in missing_cols:
+        X[c] = 0
+
+    # filter to columns used in training
+    X = X[train_columns]
+
+    return X
 
 
 if __name__ == "__main__":
@@ -18,3 +59,41 @@ if __name__ == "__main__":
             format='%(asctime)s %(levelname)-8s %(message)s',
             level=logging.INFO,
             datefmt='%Y-%m-%d %H:%M:%S')
+
+    pickle_path = os.path.join(dirname, '../../models')
+
+    # prep test data
+    print('Preping test data...')
+    df = prepare_data(train=False)
+
+    # load train columns
+    print('Loading training columns...')
+    train_columns = load_pickle('train_columns.pk')
+
+    X = get_missing_cols(train_columns, df)
+    X.to_csv('../../data/processed/test.csv', index=False)
+    logging.info("Done prepping and saving test data.")
+
+    print('Loading label definitions...')
+    definitions = load_pickle('level_mappings.pk')
+
+    # load pickled model
+    print('Loading serialized model...')
+    clf = load_pickle("lgbm_model.pkl")
+    logging.info("Classifier loaded.")
+
+    # predict
+    print('Predicting...')
+    y_pred = clf.predict(X)
+
+    # get max probability for each prediction
+    y_pred = [np.argmax(line) for line in y_pred]
+    
+    # convert back to labels
+    pred_labels = definitions[y_pred]
+    df['level'] = pred_labels
+
+    logging.info("Predictions complete")
+
+    df[['level']].to_csv('../../data/results/predictions.csv', index=False)
+    print('Results saved.')
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,10 +3,94 @@ import logging
 import numpy as np
 import os
 import pandas as pd
+import pickle
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+
+
+def prepare_data(train=True, input_path='../../data/interim/', lgbm=True):
+    """Prepare data for input into model by dropping and imputing data.
+
+    Args:
+        train (bool): boolean correspoing to train or predict
+        input_path (str): path to cleaned and featurized data
+        lgbm (bool): boolean correspoinding to model type
+    Returns:
+        Dataframe with relevant columns
+
+    """
+    if train:
+        ext = '_train.csv'
+        desc = 'train'
+    else:
+        ext = '_test.csv'
+        desc = 'predict'
+
+    # read interim data
+    print('Loading ' + desc + ' data...')
+    dem = pd.read_csv(input_path + 'demographics' + ext)
+    exp = pd.read_csv(input_path + 'exposure' + ext)
+    transfer = pd.read_csv(input_path + 'transfer' + ext)
+    
+    # combine dfs together
+    df = dem.merge(exp, how='left', on='EXCHANGE_ACCOUNT_ID').merge(
+            transfer, how='left', left_on='EXCHANGE_ACCOUNT_ID', right_on='ACCOUNT_ID')
+    
+    # define columns to drop
+    drop_list = [x for x in df.columns if 'CURRENCY_sum' in x] # string concats
+    drop_list.extend(['BIRTH_YEAR', 'EXCHANGE_ACCOUNT_ID', 'ACCOUNT_ID', 'OCCUPATION', 'dummy', 'CREATED_AT', 'FIRST_VERIFIED_AT'])
+    cat_list = ['OCCUPATION_GROUP', 'STATE_CODE', 'COUNTRY_CODE'] 
+
+    if lgbm:
+        # convert to categorical
+        df[cat_list] = df[cat_list].astype('category') 
+    else:
+        # drop categorical (other option is one-hot encode)
+        drop_list.extend(cat_list)   
+        # fill na with column mean      
+        df = df.fillna(df.mean())
+        
+    df.drop(drop_list, axis=1, inplace=True)
+    
+    return df
+
+
+def train_lgb_clf(X, y):
+    """Train light gbm classifier.
+
+    Args:
+        X (dataframe): feature set
+        y (series): dependent variable
+    Returns:
+        trained classifier
+
+    """
+    # format data for lgbm
+    train_data = lgb.Dataset(X, label=y)
+
+    # model parameters (needs further tuning)
+    parameters = {
+        'objective': 'multiclass',
+        'num_class': 9,
+        'metric': 'multi_logloss',
+        'is_unbalance': 'true',
+        'boosting': 'gbdt',
+        'num_leaves': 30,
+        'feature_fraction': 0.5,
+        'bagging_fraction': 0.5,
+        'bagging_freq': 20,
+        'learning_rate': 0.05,
+        'boost_from_average': True,
+        'verbose': -1,
+        'seed': 100
+    }
+    
+    # train model using specified parameters
+    clf = lgb.train(parameters, train_data)
+
+    return clf
 
 
 if __name__ == "__main__":
@@ -18,3 +102,46 @@ if __name__ == "__main__":
             format='%(asctime)s %(levelname)-8s %(message)s',
             level=logging.INFO,
             datefmt='%Y-%m-%d %H:%M:%S')
+
+    df = prepare_data()
+    df.to_csv('../../data/processed/train.csv', index=False)
+    logging.info("Done prepping and saving train data.")
+
+    # Creating the dependent variable class
+    df.level, definitions = pd.factorize(df['level'])
+    # df.level = factor[0]
+    # definitions = factor[1]
+
+    # separate into dependent and independent
+    y = df.pop('level')
+    X = df
+    columns = X.columns
+
+    # train model
+    print('Training model...')
+    clf = train_lgb_clf(X, y)
+    logging.info("Model training complete.")
+
+    # save pickles
+    print('Saving pickles...')
+    pickle_path = os.path.join(dirname, '../../models')
+
+    Pkl_Filename = "lgbm_model.pkl"  
+    with open(os.path.join(pickle_path, Pkl_Filename), 'wb') as file:  
+        pickle.dump(clf, file)
+    logging.info("Model pickled and saved.")
+
+    # Save trained columns as pickle for later use
+    filename = 'train_columns.pk'
+    with open(os.path.join(pickle_path, filename), 'wb') as file:
+        pickle.dump(columns, file)
+    file.close()
+    logging.info("Save trained columns as pickle.")
+
+    # Save level definitons
+    filename = 'level_mappings.pk'
+    with open(os.path.join(pickle_path, filename), 'wb') as file:
+        pickle.dump(definitions, file)
+    file.close()
+    logging.info("Save level definitions.")
+
